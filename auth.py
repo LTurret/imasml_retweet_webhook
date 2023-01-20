7cf129a002cebffcb8092e52c5669a84f9b275a5
LTurret <a0919329@gmail.com>
2023-01-20 23:56:36 +0800

    fix a little bug

--- auth.py
+++ auth.py
@@ -6,7 +6,7 @@ def secrets() -> list:
         CONSUMER_KEY = secret["CONSUMER_KEY"]
         CONSUMER_KEY_SECRET = secret["CONSUMER_KEY_SECRET"]
         ACCESS_TOKEN = secret["ACCESS_TOKEN"]
-        ACCESS_TOKEN_SECRET = secret["ACCESS_TOKEN_SECRET"],
+        ACCESS_TOKEN_SECRET = secret["ACCESS_TOKEN_SECRET"]
         WEBHOOK_URL = secret["WEBHOOK_URL"]
     secrets = [CONSUMER_KEY, CONSUMER_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET, WEBHOOK_URL]
     return secrets
